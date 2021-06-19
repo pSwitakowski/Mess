2680c9e65b0c1404b8df15f39b038cd11ae985d0
pSwitakowski <piotr.switakowski@student.wat.edu.pl>
2021-06-19 23:14:34 +0200

    Added logout option on profile fragment

--- app/src/main/java/com/example/mess/home/HomeFragment.kt
+++ app/src/main/java/com/example/mess/home/HomeFragment.kt
@@ -1,15 +1,13 @@
 package com.example.mess.home
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.mess.R
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeFragment : Fragment() {
-
     private val homeViewmodel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
--- app/src/main/java/com/example/mess/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/mess/profile/ProfileFragment.kt
@@ -2,25 +2,45 @@ package com.example.mess.profile
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.mess.R
 import com.example.mess.data.User
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 
 class ProfileFragment : Fragment() {
     private val PROFILE_DEBUG = "PROFILE_DEBUG"
-
+    private val fbAuth = FirebaseAuth.getInstance()
     private val profileViewmodel by viewModels<ProfileViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.logout_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.home_logout_item -> {
+                fbAuth.signOut()
+                requireActivity().finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
@@ -33,6 +53,11 @@ class ProfileFragment : Fragment() {
 
     private fun bindUserData(user: User) {
         Log.d(PROFILE_DEBUG, user.toString())
+        profile_email_tv.setText(user.email)
+        profile_name_tv.setText(user.name)
+        //details section
+        profile_details_email_tv.setText(user.email)
+        profile_details_name_tv.setText(user.name)
     }
 
 }
