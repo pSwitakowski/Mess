cccedadc6c7c37d6bfb886376a630b5a38629da7
pSwitakowski <piotr.switakowski@student.wat.edu.pl>
2021-06-19 19:29:23 +0200

    theme update

--- app/src/main/java/com/example/mess/data/User.kt
+++ app/src/main/java/com/example/mess/data/User.kt
@@ -1,5 +1,6 @@
 package com.example.mess.data
 
-data class User(val uid: String? = null,
-                val email: String? = null
+data class User(val email: String? = null,
+                val name: String? = null,
+                val uid: String? = null
                 )
